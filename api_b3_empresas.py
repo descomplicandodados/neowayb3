842a4c362ab2e0851165b54a1553b513eab664cd
DaniloFerreira <gmnmidias@gmail.com>
2024-05-25 15:22:56 -0300

    fastapi teste

--- api_b3_empresas.py
+++ api_b3_empresas.py
@@ -1,12 +1,18 @@
+import requests
 from fastapi import FastAPI
 from google.cloud import bigquery
+import json
+import pandas as pd
+import uvicorn
 
-json_path = r"C:\Users\danil\OneDrive\Área de Trabalho\neoway\neoway_b3\neowayb3-e91ece58c23c.json"
+json_url = 'https://storage.googleapis.com/carteira1/neowayb3-e91ece58c23c.json'
+response = requests.get(json_url)
+json_path = response.json()
 
 servidor = FastAPI()
 
 # Configurar cliente BigQuery com as credenciais fornecidas
-client = bigquery.Client.from_service_account_json(json_path)
+client = bigquery.Client.from_service_account_info(json_path)
 
 # Endpoint para consultar dados na tabela b3_empresas no BigQuery
 @servidor.get("/b3_empresas")
@@ -19,9 +25,9 @@ async def query_b3_empresas():
 
     # Enviar consulta para o BigQuery
     query_job = client.query(query)
+    df = await query_job.result()
 
-    # Aguardar resultados da consulta
-    results = query_job.result()
+    return df.to_dict(orient='records')
 
-    # Retornar resultados
-    return {"results": [dict(row) for row in results]}
+if __name__ == "__main__":
+    uvicorn.run(servidor, host="127.0.0.1", port=8000)
