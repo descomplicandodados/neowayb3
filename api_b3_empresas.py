78091225030a45b66f38059a9dea6eb17cb112fd
DaniloFerreira <gmnmidias@gmail.com>
2024-05-26 01:19:09 -0300

    fast api atualizado e funcionando

--- api_b3_empresas.py
+++ api_b3_empresas.py
@@ -5,29 +5,32 @@ import json
 import pandas as pd
 import uvicorn
 
+# Carrega as credenciais do Google Cloud
 json_url = 'https://storage.googleapis.com/carteira1/neowayb3-e91ece58c23c.json'
 response = requests.get(json_url)
 json_path = response.json()
 
-servidor = FastAPI()
-
-# Configurar cliente BigQuery com as credenciais fornecidas
+# Configura o cliente BigQuery com as credenciais fornecidas
 client = bigquery.Client.from_service_account_info(json_path)
 
-# Endpoint para consultar dados na tabela b3_empresas no BigQuery
-@servidor.get("/b3_empresas")
-async def query_b3_empresas():
-    # Consulta SQL para selecionar todos os dados da tabela b3_empresas
-    query = """
-    SELECT *
-    FROM `neowayb3.silver.b3_empresas`
-    """
+# Consulta SQL para selecionar todos os dados da tabela b3_empresas
+query = """
+SELECT *
+FROM `neowayb3.silver.b3_empresas`
+"""
 
-    # Enviar consulta para o BigQuery
-    query_job = client.query(query)
-    df = await query_job.result()
+# Envia a consulta para o BigQuery
+query_job = client.query(query)
+df = query_job.result().to_dataframe()
 
-    return df.to_dict(orient='records')
+# Converter DataFrame para um dicionário
+df_dict = df.to_dict(orient='records')
 
-if __name__ == "__main__":
-    uvicorn.run(servidor, host="127.0.0.1", port=8000)
+# Criar o servidor FastAPI
+app = FastAPI()
+
+# Definir um endpoint para obter os dados da tabela b3_empresas
+@app.get("/b3_empresas")
+async def get_b3_empresas():
+    return df_dict
+
